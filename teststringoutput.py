9a649943c07667a444a33dcbd6ca2218b40e04fa
Tong Wang <tow087@mail.usask.ca>
2018-06-18 13:14:11 -0600

    stringout

---
+++ teststringoutput.py
@@ -0,0 +1,7 @@
+print('''# -*- coding: utf-8 -*-
+... n = 123
+... f = 456.789
+... s1 = 'Hello, world'
+... s2 = \'Hello,\\\'Adam\\\'\'
+... s3 = r\'Hellp, \"Bart\"\'
+... s4 = r\'\'\'Hello,\nLisa!\'\'\'''')
